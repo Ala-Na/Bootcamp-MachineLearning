9e2f79c50bcd8d061c6f941a4aff40f1f366693d
Nadege ALAVOINE <anadege@e2r2p19.clusters.42paris.fr>
2021-11-26 19:53:44 +0100

    started extract datas for M01ex04

--- M01/ex04/linear_model.py
+++ M01/ex04/linear_model.py
@@ -1,3 +1,18 @@
 import numpy as np
+import os
+import pandas as pd
 from my_linear_regression import MyLinearRegression as MyLR
 
+def extract_vectors(filename):
+    assert os.path.isfile(filename)
+    datas = np.asarray(pd.read_csv(filename))
+    x = datas[:, 1].reshape(-1, 1)
+    y = datas[:, 2].reshape(-1, 1)
+    return [x, y]
+
+if __name__ == '__main__':
+    vectors = extract_vectors("./are_blue_pills_magics.csv")
+    x = vectors[0]
+    y = vectors[1]
+    print(x)
+    print(y)
