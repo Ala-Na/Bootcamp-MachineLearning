29adf907f5fb66ff75ac034e50f605b6e4a687f6
anadege <anadege@student.42.fr>
2022-07-31 18:20:05 +0200

    added some checks

--- M01/ex01/vec_gradient.py
+++ M01/ex01/vec_gradient.py
@@ -3,26 +3,30 @@ import numpy as np
 #NO FOR LOOP
 
 def gradient(x, y, theta):
-    if not isinstance(x, np.ndarray) or x.ndim != 2 or x.shape[1] != 1:
+    """Computes a gradient vector from three non-empty numpy.array, without any for loop.
+    The three arrays must have compatible shapes.
+    Args:
+    x: has to be a numpy.array, a matrix of shape m * 1.
+    y: has to be a numpy.array, a vector of shape m * 1.
+    theta: has to be a numpy.array, a 2 * 1 vector.
+    Return:
+    The gradient as a numpy.ndarray, a vector of dimension 2 * 1.
+    None if x, y, or theta is an empty numpy.ndarray.
+    None if x, y and theta do not have compatible dimensions.
+    Raises:
+    This function should not raise any Exception.
+    """
+    if not isinstance(x, np.ndarray) or not np.issubdtype(x.dtype, np.number) or x.ndim != 2 or x.shape[1] != 1 or x.shape[0] == 0:
         return None
-    if not isinstance(y, np.ndarray) or y.ndim != 2 or y.shape[1] != 1:
+    if not isinstance(y, np.ndarray) or not np.issubdtype(y.dtype, np.number) or y.ndim != 2 or y.shape[1] != 1 or y.shape[0] == 0:
         return None
-    if not isinstance(theta, np.ndarray) or theta.shape != (2, 1):
+    if not isinstance(theta, np.ndarray) or not np.issubdtype(theta.dtype, np.number) or theta.shape != (2, 1):
         return None
-    X = np.insert(x, 0, 1.0, axis = 1)
-    X_T = X.transpose()
-    y_hat = np.dot(X, theta)
-    return np.dot(X_T, y_hat - y) / x.shape[0]
-
-x = np.array([[12.4956442],[ 21.5007972],[ 31.5527382],[ 48.9145838],[ 57.5088733]])
-y = np.array([[37.4013816],[ 36.1473236],[ 45.7655287],[ 46.6793434],[ 59.5585554]])
-
-theta1 = np.array([[2],[ 0.7]])
-print(gradient(x, y, theta1))
-print("\n")
-#array([[-19.0342574],[-586.66875564])
-
-
-theta2 = np.array([[1],[ -0.4]])
-print(gradient(x, y, theta2))
-#array([[-57.86823748],[-2230.12297889]])
+    try:
+        X = np.insert(x, 0, 1.0, axis = 1)
+        X_T = X.transpose()
+        y_hat = np.dot(X, theta)
+        return np.dot(X_T, y_hat - y) / x.shape[0]
+    except:
+        print("Something went wrong")
+        return None
--- M01/ex02/fit.py
+++ M01/ex02/fit.py
@@ -1,52 +1,71 @@
 import numpy as np
 
 def predict(x, theta):
-    if not isinstance(x, np.ndarray) or x.ndim != 2 or x.shape[1] != 1:
+    if not isinstance(x, np.ndarray) or not np.issubdtype(x.dtype, np.number) or x.ndim != 2 or x.shape[1] != 1 or x.shape[0] == 0:
         return None
-    elif not isinstance(theta, np.ndarray) or theta.ndim != 2 or theta.shape[0] != 2 or theta.shape[1] != 1:
+    elif not isinstance(theta, np.ndarray) or not np.issubdtype(theta.dtype, np.number) or theta.shape != (2, 1):
         return None
-    X = np.insert(x, 0, 1.0, axis=1)
-    return np.dot(X, theta)
+    try:
+        X = np.insert(x, 0, 1.0, axis=1)
+        return np.dot(X, theta)
+    except:
+        return None
 
 def gradient(x, y, theta):
-    if not isinstance(x, np.ndarray) or x.ndim != 2 or x.shape[1] != 1:
+    if not isinstance(x, np.ndarray) or not np.issubdtype(x.dtype, np.number) or x.ndim != 2 or x.shape[1] != 1 or x.shape[0] == 0:
         return None
-    if not isinstance(y, np.ndarray) or y.ndim != 2 or y.shape[1] != 1:
+    if not isinstance(y, np.ndarray) or not np.issubdtype(y.dtype, np.number) or y.ndim != 2 or y.shape[1] != 1 or y.shape[0] == 0:
         return None
-    if not isinstance(theta, np.ndarray) or theta.shape != (2, 1):
+    if not isinstance(theta, np.ndarray) or not np.issubdtype(theta.dtype, np.number) or theta.shape != (2, 1):
         return None
-    X = np.insert(x, 0, 1.0, axis = 1)
-    X_T = X.transpose()
-    y_hat = np.dot(X, theta)
-    return np.dot(X_T, y_hat - y) / x.shape[0]
+    try:
+        X = np.insert(x, 0, 1.0, axis = 1)
+        X_T = X.transpose()
+        y_hat = np.dot(X, theta)
+        return np.dot(X_T, y_hat - y) / x.shape[0]
+    except:
+        return None
 
 def fit_(x, y, theta, alpha, max_iter):
-    if not isinstance(x, np.ndarray) or x.ndim != 2 or x.shape[1] != 1:
+    """
+    Description:
+    Fits the model to the training dataset contained in x and y.
+    Args:
+    x: has to be a numpy.ndarray, a vector of dimension m * 1: (number of training examples, 1).
+    y: has to be a numpy.ndarray, a vector of dimension m * 1: (number of training examples, 1).
+    theta: has to be a numpy.ndarray, a vector of dimension 2 * 1.
+    alpha: has to be a float, the learning rate
+    max_iter: has to be an int, the number of iterations done during the gradient descent
+    Returns:
+    new_theta: numpy.ndarray, a vector of dimension 2 * 1.
+    None if there is a matching dimension problem.
+    Raises:
+    This function should not raise any Exception.
+    """
+    if not isinstance(x, np.ndarray) or not np.issubdtype(x.dtype, np.number) or x.ndim != 2 or x.shape[1] != 1 or x.shape[0] == 0:
         return None
-    if not isinstance(y, np.ndarray) or y.ndim != 2 or y.shape[1] != 1:
+    if not isinstance(y, np.ndarray) or not np.issubdtype(y.dtype, np.number) or y.ndim != 2 or y.shape[1] != 1 or y.shape[0] == 0:
         return None
-    if not isinstance(theta, np.ndarray) or theta.shape != (2, 1):
+    if not isinstance(theta, np.ndarray) or not np.issubdtype(theta.dtype, np.number) or theta.shape != (2, 1):
         return None
     if not isinstance(alpha, float) or not isinstance(max_iter, int):
         return None
-    new_theta = theta
-    for i in range(0, max_iter):
-        curr_gradient = gradient(x, y, new_theta)
-        theta_zero = (float)(new_theta[0][0] - alpha * curr_gradient[0][0])
-        theta_one = (float)(new_theta[1][0] - alpha * curr_gradient[1][0])
-        new_theta = np.asarray([theta_zero, theta_one]).reshape((2, 1))
-    return new_theta
-        
-    
+    try:
+        new_theta = theta
+        for i in range(0, max_iter):
+            curr_gradient = gradient(x, y, new_theta)
+            theta_zero = (float)(new_theta[0][0] - alpha * curr_gradient[0][0])
+            theta_one = (float)(new_theta[1][0] - alpha * curr_gradient[1][0])
+            new_theta = np.asarray([theta_zero, theta_one]).reshape((2, 1))
+        return new_theta
+    except:
+        return None
 
-x = np.array([[12.4956442], [21.5007972], [31.5527382], [48.9145838], [57.5088733]])
-y = np.array([[37.4013816], [36.1473236], [45.7655287], [46.6793434], [59.5585554]])
-theta = np.array([[1],[ 1]])
+# You can create more training data by generating an x array
+# with random values and computing the corresponding y vector as
+# a linear expression of x. You can then fit a model on this
+# artificial data and find out if it comes out with the same θ
+# coefficients that first you used.
 
-theta1 = fit_(x, y, theta, alpha=5e-6, max_iter=15000)
-print(theta1)
-#array([[1.40709365], [1.1150909 ]])
-
-print("\n")
-print(predict(x, theta1))
-#array([[15.3408728 ],[25.38243697],[36.59126492],[55.95130097],[65.53471499]])
+# It is possible that θ0 and θ1 become "nan". In that case, it
+# means you probably used a learning rate that is too large.
