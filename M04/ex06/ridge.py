7b4e397e8c380b96838dfb1bcafe4875bd63f358
anadege <anadege@student.42.fr>
2022-08-10 23:42:12 +0200

    m04 ex06 to continue

--- M04/ex06/ridge.py
+++ M04/ex06/ridge.py
@@ -1,14 +1,15 @@
+from mylinearregression import MyLinearRegression as MyLR
 
-
-class MyRidge(ParentClass):
-"""
-Description:
-My personnal ridge regression class to fit like a boss.
-"""
-def __init__(self, thetas, alpha=0.001, max_iter=1000, lambda_=0.5):
-self.alpha = alpha
-self.max_iter = max_iter
-self.thetas = thetas
-self.lambda_ = lambda_
-... Your code here ...
-... other methods ...
+class MyRidge(MyLR):
+	"""
+	Description:
+		My personnal ridge regression class to fit like a boss.
+	"""
+	
+	def __init__(self, thetas, alpha=0.001, max_iter=1000, lambda_=0.5):
+	self.alpha = alpha
+	self.max_iter = max_iter
+	self.thetas = thetas
+	self.lambda_ = lambda_
+	... Your code here ...
+	... other methods ...
