f3bdbb4db4344340d4e354190f776118b2b7e052
anadege <anadege@student.42.fr>
2022-08-10 16:01:42 +0200

    finished ex07m03

--- M03/ex07/mono_log.py
+++ M03/ex07/mono_log.py
@@ -4,53 +4,109 @@ import os
 from data_spliter import data_spliter
 import argparse
 from my_logistic_regression import MyLogisticRegression as MyLR
+import itertools
+import matplotlib.pyplot as plt
 
 def extract_datas(filename):
     assert os.path.isfile(filename)
     try:
         datas = pd.read_csv(filename)
         datas_array = np.asarray(datas)
-        return datas_array
+        return datas, datas_array
     except:
         print('Something went wrong with extract_datas function')
         return None
 
-def get_splitted_datas(zipcode):
+def get_splitted_datas(zipcode=None):
 	# Retrieve datas from files
-	x = extract_datas('solar_system_census.csv')[:, 1:]
-	y = (extract_datas('solar_system_census_planets.csv'))[:, 1].reshape(-1, 1)
+	x_datas, x = extract_datas('solar_system_census.csv')
+	x = x[:, 1:]
+	y_datas, y = extract_datas('solar_system_census_planets.csv')
+	y = y[:, 1].reshape(-1, 1)
 	# Check for correct shapes
 	if y.shape != (x.shape[0], 1):
 		print("Error in shapes !")
 		exit(1)
 	# Replace zipcode inside y by 1 (is expected zipcode) and 0 (is another zipcode)
-	y = np.select([y == zipcode, y != zipcode], [1, 0], y)
+	if zipcode != None:
+		y = np.select([y == zipcode, y != zipcode], [1, 0], y)
 	# Split data set in training and testing sets
 	x_train, x_test, y_train, y_test = data_spliter(x, y, 0.8)
-	return x_train, x_test, y_train, y_test	
+	return x_datas, x, y, x_train, x_test, y_train, y_test	
 
-def train_mono_model(zipcode):
+
+def draw_sub_spots(ax, x, y, label, color, size):
+	# Scatter spots on x and y coordinates
+    try:
+        ax.scatter(x, y, size, label=label, color=color, zorder=2)
+    except:
+        print('Something went wrong in draw_sub_spots function')
+
+def get_mono_sub_graph(ax, x, y_hat, labels, axis_features, colors):
+	try:
+		# Draw other planets spots
+		draw_sub_spots(ax, x[axis_features[0]].loc[(y_hat == 1)], x[axis_features[1]].loc[(y_hat == 1)], labels[0], colors[0], 25)
+		# Draw current planet spots
+		draw_sub_spots(ax, x[axis_features[0]].loc[(y_hat == 0)], x[axis_features[1]].loc[(y_hat == 0)], labels[1], colors[1], 15)
+		# Set features legends
+		ax.set_xlabel(axis_features[0])
+		ax.set_ylabel(axis_features[1])
+		ax.legend()
+	except:
+		print('Something went wrong in get_sub_graph function')
+
+def draw_mono_scatter_plots(zipcode, x, y_hat):
+	# Create pair of datas
+	planets = ['Venus', 'Earth', 'Mars', 'Belt']
+	planets_colors = ['green', 'blue', 'red', 'yellow']
+	labels = [planets[zipcode], 'Other planets']
+	colors = [planets_colors[zipcode], 'grey']
+	fig, ax = plt.subplots(1, 3)
+	for i, pair in enumerate(itertools.combinations(['weight','height','bone_density'], 2)):
+		get_mono_sub_graph(ax[i], x, y_hat, labels, [pair[0], pair[1]], colors)
+	plt.show()
+
+
+def train_mono_model(zipcode, x_original, x_train, x_test, y_train, y_test):
 	# Set model
-	mono_lor = MyLR(np.array([[1.0], [1.0], [1.0], [1.0]]), alpha=0.0001, max_iter=10000)
-	
-	# Get datas
-	x_train, x_test, y_train, y_test = get_splitted_datas(zipcode)
+	# Recommend: alpha=0.0001, max_iter=500000
+	mono_lor = MyLR(np.array([[1.0], [1.0], [1.0], [1.0]]), alpha=0.0001, max_iter=500000)
 
 	# Train model
-	print("Training model... (may take some time)")
+	print("Training model for planet {}... (may take some time)".format(zipcode))
 	mono_lor.fit_(x_train, y_train)
 	print("Model trained !\n")
 
-	# Calculate loss and predictions
+	# Calculate loss and predictions in train/test/global set
 	x_fullset = np.concatenate([x_train, x_test])
 	y_fullset = np.concatenate([y_train, y_test])
-	predictions = np.round(mono_lor.predict_(x_fullset))
-	loss = mono_lor.loss_(y_fullset, predictions)
 	print("Thetas: \033[34m{}\033[0m".format(mono_lor.theta.reshape(1, -1)[0]))
-	print("Loss: \033[34m{}\033[0m".format(loss))
-	print("Proportion of correct predicted values: \033[34m{}%\033[0m".format((np.sum(predictions == y_fullset) / y_fullset.size) * 100))
-	expected = (predictions == y_fullset)
-	# Display predictions
+
+	print("Proportion of correct predicted values:")
+
+	y_hat_train = mono_lor.predict_(x_train)
+	y_hat_rounded = np.round(y_hat_train)
+	correctness = (np.sum(y_hat_rounded == y_train) / y_train.size)
+	print("Train set: \033[34m{}%\033[0m".format(correctness * 100))
+
+	y_hat_test = mono_lor.predict_(x_test)
+	y_hat_rounded = np.round(y_hat_test)
+	y_hat_rounded = np.round(mono_lor.predict_(x_test))
+	correctness = (np.sum(y_hat_rounded == y_test) / y_test.size)
+	print("Test set: \033[34m{}%\033[0m".format(correctness * 100))
+	
+	y_hat_rounded = np.round(mono_lor.predict_(x_fullset))
+	correctness = (np.sum(y_hat_rounded == y_fullset) / y_fullset.size)
+	print("Global set: \033[34m{}%\033[0m".format(correctness * 100))
+
+	loss = mono_lor.loss_(y_fullset, y_hat_rounded)
+	print("Loss on global set: \033[34m{}\033[0m".format(loss))
+
+
+	# Draw scatter plots for each pair of features
+	y_hat_global = mono_lor.predict_(x_original)
+	y_hat_rounded = np.round(y_hat_global)
+	return y_hat_train, y_hat_test, y_hat_global, y_hat_rounded
 
 
 if __name__ == '__main__':
@@ -60,5 +116,16 @@ if __name__ == '__main__':
 	if args.zipcode[0] > 3 or args.zipcode[0] < 0:
 		parser.print_help()
 		exit(1)
-	train_mono_model(args.zipcode[0])
 
+	print('\033[92mMONOCLASS LOGISTIC REGRESSION\033[0m')
+	print('\033[92m------------------------------\033[0m')
+
+	# Get datas
+	# We keep x_datas (pandas) and x_original (before split) to draw scatter plots later
+	x_datas, x_original, y_original, x_train, x_test, y_train, y_test = get_splitted_datas(args.zipcode[0])
+
+	# Train model
+	y_hat_train, y_hat_test, y_hat_global, y_hat_rounded = train_mono_model(args.zipcode[0], x_original, x_train, x_test, y_train, y_test)
+
+	# Draw model
+	draw_mono_scatter_plots(args.zipcode[0], x_datas, y_hat_rounded)
---
+++ M03/ex07/multi_log.py
@@ -0,0 +1,78 @@
+from mono_log import *
+
+def get_multi_sub_graph(ax, x, y_hat, labels, axis_features, colors):
+	try:
+		# Draw Venus spots
+		draw_sub_spots(ax, x[axis_features[0]].loc[(y_hat == 0)], x[axis_features[1]].loc[(y_hat == 0)], labels[0], colors[0], 15)
+		# Draw Earth spots
+		draw_sub_spots(ax, x[axis_features[0]].loc[(y_hat == 1)], x[axis_features[1]].loc[(y_hat == 1)], labels[1], colors[1], 15)
+		# Draw Mars spots
+		draw_sub_spots(ax, x[axis_features[0]].loc[(y_hat == 2)], x[axis_features[1]].loc[(y_hat == 2)], labels[2], colors[2], 15)
+		# Draw Belt spots
+		draw_sub_spots(ax, x[axis_features[0]].loc[(y_hat == 3)], x[axis_features[1]].loc[(y_hat == 3)], labels[3], colors[3], 15)
+		# Set features legends
+		ax.set_xlabel(axis_features[0])
+		ax.set_ylabel(axis_features[1])
+		ax.legend()
+	except:
+		print('Something went wrong in get_sub_graph function')
+
+def draw_multi_scatter_plots(x, y_hat):
+	planets = ['Venus', 'Earth', 'Mars', 'Belt']
+	planets_colors = ['green', 'blue', 'red', 'yellow']
+	fig, ax = plt.subplots(1, 3)
+	# Create pair of datas and draw plot accordingly
+	for i, pair in enumerate(itertools.combinations(['weight','height','bone_density'], 2)):
+		get_multi_sub_graph(ax[i], x, y_hat, planets, [pair[0], pair[1]], planets_colors)
+	plt.show()
+
+def train_multi_model(x_original, y_original, x_train, x_test, y_train, y_test):
+	planets_y_hat_train = []
+	planets_y_hat_test = []
+	planets_y_hat_global = []
+
+	print('\n\033[92mFirst step: Perform monoclass logitic regression for each planet\033[0m\n')
+
+	for planet_idx in range(0, 4):
+		print("--------------------------")
+		y_train_planet = np.select([y_train == planet_idx, y_train != planet_idx], [1, 0], y_train)
+		y_test_planet = np.select([y_test == planet_idx, y_test != planet_idx], [1, 0], y_test)
+		y_hat_train, y_hat_test, y_hat_global, y_hat_rounded = train_mono_model(planet_idx, x_original, x_train, x_test, y_train_planet, y_test_planet)
+		planets_y_hat_train.append(y_hat_train)
+		planets_y_hat_test.append(y_hat_test)
+		planets_y_hat_global.append(y_hat_global)
+		print("--------------------------")
+		print('\n')
+
+	print('\033[92mSecond step: Deduce planet classification for each citizen\033[0m\n')
+	
+	planets_predictions_train = np.argmax(planets_y_hat_train, axis=0).reshape(-1, 1)
+	planets_predictions_test = np.argmax(planets_y_hat_test, axis=0).reshape(-1, 1)
+	planets_predictions_global = np.argmax(planets_y_hat_global, axis=0).reshape(-1, 1)
+
+	print("Proportion of correct predicted values:")
+
+	correctness = (np.sum(planets_predictions_train == y_train) / y_train.size)
+	print("Train set: \033[34m{}%\033[0m".format(correctness * 100))
+
+	correctness = (np.sum(planets_predictions_test == y_test) / y_test.size)
+	print("Test set: \033[34m{}%\033[0m".format(correctness * 100))
+
+	correctness = (np.sum(planets_predictions_global == y_original) / y_original.size)
+	print("Global set: \033[34m{}%\033[0m".format(correctness * 100))
+
+	return planets_predictions_global
+
+if __name__ == '__main__':
+	print('\033[92mMULTICLASS LOGISTIC REGRESSION\033[0m')
+	print('\033[92m------------------------------\033[0m')
+
+	# Get datas
+	# We keep x_datas (pandas) and x_original (before split) to draw scatter plots later
+	x_datas, x_original, y_original, x_train, x_test, y_train, y_test = get_splitted_datas()
+
+	# Train model
+	y_hat = train_multi_model(x_original, y_original, x_train, x_test, y_train, y_test)
+
+	# Draw scatter plots
+	draw_multi_scatter_plots(x_datas, y_hat)
